32e4df3d06755a64d606191be8777ef4884b5bdd
Oreadox <yangchen29@outlook.com>
2022-04-09 13:46:44 +0800

    完善用户管理页面

--- src/views/admin/user/UserList.vue
+++ src/views/admin/user/UserList.vue
@@ -8,7 +8,7 @@
       <el-table-column prop="department" label="院系" width="120" sortable/>
       <el-table-column label="权限">
         <template #default="scope">
-          <div v-if="scope.row['permission'].length>0">
+          <div v-if="(typeof scope.row['permission'])==='object'&&scope.row['permission'].length>0">
             <el-tag class="tag" v-if="scope.row['permission'].includes('news:newEdit')||
                                     scope.row['permission'].includes('*:*')">
               发布/编辑新闻
@@ -35,8 +35,20 @@
           <el-input v-model="search" placeholder="搜索"/>
         </template>
         <template #default="scope">
+          <el-button size="small" type="primary"
+                     @click="openPermissionDialog(scope.row['id'],scope.row['username'],scope.row['permission'])">
+            赋予权限
+          </el-button>
+          <el-popconfirm title="确定要移除该用户权限吗？" icon-color="red"
+                         @confirm="removeAllPermission(scope.row['id'])"
+                         confirmButtonText="确定" cancelButtonText="取消">
+            <template #reference>
+              <el-button size="small" type="danger">移除权限</el-button>
+            </template>
+          </el-popconfirm>
+          <div style="margin: 5px"></div>
           <el-popconfirm title="确定要重置密码吗？" icon-color="blue"
-                         @confirm="resetPassword(scope.row['id'],scope.row['username'] )"
+                         @confirm="resetPassword(scope.row['id'],scope.row['username'])"
                          confirmButtonText="确定" cancelButtonText="取消">
             <template #reference>
               <el-button size="small" type="primary">重置密码</el-button>
@@ -51,6 +63,26 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog :title='title' v-model="openDialog" :width="640">
+      <el-form label-width="60px">
+        <el-form-item label="权限">
+          <el-checkbox-group v-model="currentUserPermission">
+            <el-checkbox label="news:newEdit">发布/编辑新闻</el-checkbox>
+            <el-checkbox label="news:check">审核新闻</el-checkbox>
+            <el-checkbox label="user:*">用户管理</el-checkbox>
+            <el-tooltip placement="bottom" effect="light" content="包含前述所有权限">
+              <el-checkbox label="*:*">超级管理员</el-checkbox>
+            </el-tooltip>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitPermission(currentUserId, currentUserPermission)">
+            提交
+          </el-button>
+          <el-button @click="openDialog=false">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -64,10 +96,17 @@ export default {
   data() {
     return {
       userData: [],
-      search: ''
+      search: '',
+      currentUserId: '',
+      currentUsername: '',
+      currentUserPermission: [],
+      openDialog: false,
     }
   },
   computed: {
+    title: function () {
+      return `请选择用户${this.currentUsername}的权限`
+    },
     userDataFilter: function () {
       if (this.search.length === 0) {
         return this.userData
@@ -83,7 +122,7 @@ export default {
     this.initData()
   },
   methods: {
-    initData(){
+    initData() {
       let that = this
       axios({
         method: 'get',
@@ -101,7 +140,9 @@ export default {
               createTime: moment(admin['create_time']).format("YYYY-MM-DD HH:mm:ss"),
               permission: admin['permission']
             })
+            console.log(admin['permission'])
           })
+
         } else {
           ElMessage({
             message: '获取失败',
@@ -110,7 +151,40 @@ export default {
         }
       })
     },
-    resetPassword(id, username){
+    openPermissionDialog(id, username, permission) {
+      this.currentUserId = id
+      this.currentUsername = username
+      this.currentUserPermission = permission != null ? permission.slice() : []
+      this.openDialog = true
+    },
+    submitPermission(id, permission){
+      let that = this
+      axios({
+        method: 'put',
+        url: '/admin',
+        data: {
+          id: id,
+          permission: permission
+        }
+      }).then(function (response) {
+        var respData = response['data']
+        if (Boolean(respData['status']) === true) {
+          ElMessage({
+            message: '更改成功',
+            type: 'success',
+          })
+          that.openDialog = false
+          that.userData = []
+          that.initData()
+        } else {
+          ElMessage({
+            message: respData['message'],
+            type: 'error',
+          })
+        }
+      })
+    },
+    resetPassword(id, username) {
       let password = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
       axios({
         method: 'put',
@@ -133,13 +207,10 @@ export default {
         }
       })
     },
-    deleteUser(id){
+    deleteUser(id) {
       axios({
         method: 'delete',
-        url: '/admin',
-        data: {
-          id: id
-        }
+        url: `/admin/${id}`
       }).then(function (response) {
         var respData = response['data']
         if (Boolean(respData['status']) === true) {
@@ -147,6 +218,32 @@ export default {
             message: '删除成功',
             type: 'success',
           })
+        } else {
+          ElMessage({
+            message: respData['message'],
+            type: 'error',
+          })
+        }
+      })
+    },
+    removeAllPermission(id) {
+      let that = this
+      axios({
+        method: 'put',
+        url: '/admin',
+        data: {
+          id: id,
+          permission: []
+        }
+      }).then(function (response) {
+        var respData = response['data']
+        if (Boolean(respData['status']) === true) {
+          ElMessage({
+            message: '移除成功',
+            type: 'success',
+          })
+          that.userData = []
+          that.initData()
         } else {
           ElMessage({
             message: respData['message'],
